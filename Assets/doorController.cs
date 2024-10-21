be5b1b31491f14fa26c54f832a45515be00b1876
Kaslanas <victor24274@gmail.com>
2024-10-21 17:24:50 -0400

    Animation

---
+++ Assets/doorController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorController : MonoBehaviour
+{
+[SerializeReference] Animator door;
+
+private void OnTriggerEnter(Collider other)
+{
+    if(other.CompareTag("Player")){
+       print("Open door!");
+       door.SetBool("IsOpen", true);
+    }
+}
+
+
+private void OnTriggerExit(Collider other)
+{
+    if(other.CompareTag("Player")){
+       print("Close door!");
+        door.SetBool("IsOpen", false);
+    }
+}
+
+}
